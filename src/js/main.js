024a1650264d478030bc518d8339621f546daa96
Ikenna <ikennaugwuh@gmail.com>
2016-07-26 12:06:44 -0400

    add an add controller.

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import 'angular-ui-router';
 import config from './utilities/config';
 import SERVER from './utilities/server.constant';
+import AddController from './controllers/add.controller';
 
 // Import some controllers
 import { RegisterController } from './controllers/register.controller';
@@ -13,4 +14,5 @@ angular
     .module('app', ['ui.router'])
     .config(config)
     .constant('SERVER', SERVER)
+    .controller('AddController', AddController)
     .controller('RegisterController', RegisterController);
